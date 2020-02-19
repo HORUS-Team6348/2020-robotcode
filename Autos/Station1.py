7326bcba0fa645b4465696948984489c5af2cab2
PAOK <2001.paok@gmail.com>
2020-02-19 01:55:06 -0600

    io

--- Autos/Station1.py
+++ Autos/Station1.py
@@ -8,22 +8,27 @@ class S_1:
         self.has_crossed_line = False
         self.has_arrived      = False
         self.has_shot         = False
+        self.inicial_state = self.robot.left_drivetrain_motor.getSelectedSensorPosition()
+        self.goal = -1*(4159*2)
+
+
 
     def drive(self):
         #una vuelta equivale a 4159
         #una vuelta 47.87cm
-        self.goal = (self.robot.left_drivetrain_encoder + (4159*4.))
 
         if not self.has_crossed_line:
-            if self.robot.left_drivetrain_encoder < (self.goal- 100):
-                self.robot.drivetrain.drive_with_gyro_pid(self.robot.gyro, .4)
+            if self.robot.left_drivetrain_motor.getSelectedSensorPosition() - self.inicial_state > (self.goal- 100):
+                self.robot.drivetrain.drive_with_gyro_pid(self.robot.navx, .4)
 
-            if self.robot.left_drivetrain_encoder > (self.goal + 100):
+            if self.robot.left_drivetrain_motor.getSelectedSensorPosition() -self.inicial_state < (self.goal + 100):
                 self.has_crossed_line = True
-                self.robot.drivetrain.stop()
 
-        wpilib.SmartDashboard.putNumber(keyName="auto goal", value= self.goal)
-        wpilib.SmartDashboard.putNumber(keyName="auto encoder", value= self.robot.left_drivetrain_encoder)
+        elif self.has_crossed_line == True:
+            self.robot.drivetrain.turn_with_pid(self.robot.navx, )
+
+
+        wpilib.SmartDashboard.putNumber(keyName="autogoal", value= self.goal)
 
 
 
