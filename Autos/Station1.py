c6f13110eb7f2cf6d762ff5d9db51e1893a16b3a
PAOK <2001.paok@gmail.com>
2020-02-25 00:54:52 -0600

    io

--- Autos/Station1.py
+++ Autos/Station1.py
@@ -6,10 +6,15 @@ class S_1:
         self.robot = robot
 
         self.has_crossed_line = False
+        self.has_turned       = False
         self.has_arrived      = False
         self.has_shot         = False
-        self.inicial_state = self.robot.left_drivetrain_motor.getSelectedSensorPosition()
-        self.goal = -1*(4159*2)
+        self.inicial_state = self.robot.right_drivetrain_motor.getSelectedSensorPosition()
+        self.shot_pos = 0
+        self.turning_timestamp = 0
+        self.exit_timestamp    = 0
+        self.goal = (4159*2)
+        self.exit_goal = -1*(4159*4)
 
 
 
@@ -17,18 +22,53 @@ class S_1:
         #una vuelta equivale a 4159
         #una vuelta 47.87cm
 
-        if not self.has_crossed_line:
-            if self.robot.left_drivetrain_motor.getSelectedSensorPosition() - self.inicial_state > (self.goal- 100):
-                self.robot.drivetrain.drive_with_gyro_pid(self.robot.navx, .4)
+        if not self.has_arrived:
+            if self.robot.right_drivetrain_motor.getSelectedSensorPosition() - self.inicial_state < (self.goal - 100):
+                self.robot.drivetrain.drive_with_gyro_pid(self.robot.navx, .6)
 
-            if self.robot.left_drivetrain_motor.getSelectedSensorPosition() -self.inicial_state < (self.goal + 100):
-                self.has_crossed_line = True
+            if self.robot.right_drivetrain_motor.getSelectedSensorPosition() - self.inicial_state > (self.goal + 100):
+                self.has_arrived = True
+                self.robot.drivetrain.stop()
+                self.turning_timestamp = self.robot.auto_timer.getFPGATimestamp()
+                self.shot_pos = self.robot.right_drivetrain_motor.getSelectedSensorPosition()
 
-        elif self.has_crossed_line == True:
-            self.robot.drivetrain.turn_with_pid(self.robot.navx, )
 
 
-        wpilib.SmartDashboard.putNumber(keyName="autogoal", value= self.goal)
+        elif self.has_arrived == True:
+            if wpilib.Timer.getFPGATimestamp() < self.turning_timestamp + 2:
+                self.robot.box_lift_motor.set(.6)
+
+            elif wpilib.Timer.getFPGATimestamp() > self.turning_timestamp +2 and  wpilib.Timer.getFPGATimestamp() < (self.turning_timestamp + 4) :
+                self.robot.intake_motor.set(-.8)
+
+            elif wpilib.Timer.getFPGATimestamp() > self.turning_timestamp + 4:
+                self.robot.box_lift_motor.set(0)
+                self.robot.intake_motor.set(0)
+                self.has_shot = True
+                self.exit_timestamp = self.robot.auto_timer.getFPGATimestamp()
+
+        elif self.has_shot == True:
+            wpilib.SmartDashboard.putBoolean(keyName="has_shoot", value=self.has_shot)
+
+            if wpilib.Timer.getFPGATimestamp() < self.exit_timestamp + 15:
+                self.robot.drivetrain.drive_with_gyro_pid(self.robot.navx, -.6)
+
+            elif wpilib.Timer.getFPGATimestamp() > self.exit_timestamp + 15:
+                self.robot.drivetrain.stop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
